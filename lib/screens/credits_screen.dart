624c197d25abdcc6eacf1dae2c242436ec105bef
Maria Antonia Rodrigues Vieira <mariaantoniarvieira@gmail.com>
2025-05-16 21:17:47 -0300

    tela de créditos

--- lib/screens/credits_screen.dart
+++ lib/screens/credits_screen.dart
@@ -13,74 +13,84 @@ class CreditsScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const CircleAvatar(
-              radius: 60,
-              backgroundColor: AppColors.primary,
-              child: Icon(Icons.shield, size: 60, color: Colors.white),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                'assets/images/app_icon.png',
+                fit: BoxFit.cover,
+                width: 150,
+                height: 150,
+              ),
             ),
             const SizedBox(height: 24),
             Text(
-              'Prevenção de Queimaduras',
+              'Burn Quest',
               style: Theme.of(context).textTheme.displayMedium,
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 8),
-            Text(
-              'Versão 1.0.0',
-              style: Theme.of(context).textTheme.bodyMedium,
-              textAlign: TextAlign.center,
-            ),
             const SizedBox(height: 32),
             const Text(
-              'Desenvolvido por',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              'Nome da disciplina:',
+              style: TextStyle(fontSize: 18),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 8),
             const Text(
-              'Equipe de Prevenção de Acidentes',
-              style: TextStyle(fontSize: 16),
+              'Desenvolvimento de Software',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 32),
             const Text(
-              'Consultoria Técnica',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              'Nome do professsor responsável:',
+              style: TextStyle(fontSize: 18),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 8),
             const Text(
-              'Dra. Maria Silva - Especialista em Queimaduras\nDr. João Santos - Pediatra',
-              style: TextStyle(fontSize: 16),
+              'Prof. Dr. Elvio Gilberto da Silva',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 32),
             const Text(
-              'Agradecimentos Especiais',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              'Nomes completos dos integrantes e colaboradores do grupo:',
+              style: TextStyle(fontSize: 18,),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 8),
             const Text(
-              'Hospital Infantil de Referência\nCorpo de Bombeiros\nSecretaria de Educação',
-              style: TextStyle(fontSize: 16),
+              'Abner Wallace da Costa Rodrigues\nMaria Antonia Rodrigues Vieira\nVictor Silva Murakami\nVinicius Henrique Dalaqua da Silva',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 32),
             const Text(
-              'Este aplicativo foi desenvolvido com o objetivo de educar crianças sobre a prevenção de queimaduras de forma lúdica e interativa. As informações contidas neste aplicativo são baseadas em diretrizes de segurança e prevenção de acidentes.',
+              'DESENVOLVIMENTO:',
               style: TextStyle(
                 fontSize: 14,
-                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
                 color: AppColors.textSecondary,
               ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 24),
+            const SizedBox(height: 6),
+            Image.asset(
+              'assets/images/logo_creditos/ciencia_da_computacaoLogo.jpg'
+            ),
+            const SizedBox(height: 32),
             const Text(
-              '© 2023 Todos os direitos reservados',
-              style: TextStyle(fontSize: 12, color: AppColors.textSecondary),
+              'APOIO:',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textSecondary,
+              ),
               textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 6),
+            Image.asset(
+                'assets/images/logo_creditos/coordenadoriaLogo.jpg'
             ),
             const SizedBox(height: 32),
             ElevatedButton.icon(
